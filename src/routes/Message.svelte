f2f40863c3d6359bde81f298e351766313a83b3c
marcusmmmz <marcusizabel230@gmail.com>
2023-02-21 14:46:55 -0300

    accurate emoji parsing

--- src/routes/Message.svelte
+++ src/routes/Message.svelte
@@ -6,14 +6,63 @@
 
 	$: author = $usernameStore[message.author] ?? message.author;
 
-	let content = message.content.replace(
-		":pexe:",
-		'<img src="https://cdn.discordapp.com/emojis/1048253883039875122.png?size=24"></img>'
-	);
+	const emojis: Record<string, string> = {
+		":pexe:": "https://cdn.discordapp.com/emojis/1048253883039875122.png",
+		":anjo:": "https://cdn.discordapp.com/emojis/1037427525690871899.png"
+	};
+
+	function parseEmojis(content: string) {
+		// this regex splits on colons but also maintains them
+		let list = content.split(/(:)/g).filter((v) => v != "");
+
+		let newList = [];
+		let i = 0;
+
+		while (list.length > i) {
+			let element = list[i];
+
+			if (
+				element == ":" &&
+				list[i + 1] != undefined &&
+				list[i + 1] != ":" &&
+				list[i + 2] != undefined &&
+				list[i + 2] == ":"
+			) {
+				newList.push(element + list[i + 1] + list[i + 2]);
+				i += 3;
+			} else {
+				newList.push(element);
+				i++;
+			}
+		}
+
+		return newList;
+	}
 </script>
 
 <div><strong>{author}</strong></div>
-<div>{@html content}</div>
+<div>
+	{#each parseEmojis(message.content) as piece}
+		{#if emojis[piece]}
+			<img
+				class={message.content == `${piece}` ? "big emoji" : "emoji"}
+				src={emojis[piece]}
+				alt={piece}
+			/>
+		{:else}
+			{piece}
+		{/if}
+	{/each}
+</div>
 
 <style>
+	.emoji {
+		vertical-align: top;
+		width: 22px;
+		height: auto;
+	}
+
+	.big.emoji {
+		width: 4em;
+	}
 </style>
